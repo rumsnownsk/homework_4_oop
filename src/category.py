e24860f8dec9d02e8077fe49104f454172f13d52
rum <rum@example.com>
2026-08-27 20:09:40 +0700

    fix into category.py def add_product

--- src/category.py
+++ src/category.py
@@ -2,20 +2,36 @@ from src.product import Product
 
 
 class Category:
+    product_count = 0
     category_count = 0
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
         self.__products = products if products else []
-        Category.category_count += 1
+
+        # задаём кол-во Продуктов у атрибута экземпляра класса Category
         self.product_count = len(self.__products)
+
+        # задаём кол-во Продуктов у атрибута класса Category
+        Category.product_count += len(self.__products)
+
+        # задаём общее кол-во Категорий
+        Category.category_count += 1
+
 
     def add_product(self, new_product: Product):
         if not new_product:
             raise ValueError("Ошибка. не передан новый продукт")
         self.__products.append(new_product)
+
+        # если добавляем продукт в конкретную категорию то увеличиваем на 1 (единицу)
         self.product_count += 1
+
+        # эта строчка добавляет 1(единицу) аж к единому неповторимому классу Category
+        # в какое то волшебное невидимое внутреннее хранилище
+        Category.product_count += 1
+
         print(f"Новый продукт <{new_product.name}> успешно добавлен для категории <{self.name}> \n")
 
     @property
